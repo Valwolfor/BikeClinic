199b8e6d096fe8e1f34d90f144d735746c52dbe4
Valwolfor <diegofelipe.gallo@gmail.com>
2023-11-29 20:30:56 -0500

    estado agrupado para uno

--- src/main/resources/static/JS/home_1.js
+++ src/main/resources/static/JS/home_1.js
@@ -1,4 +1,4 @@
-//Para llamar métodos. 
+// //Para llamar métodos.
 
 $(document).ready(function () {
 
@@ -7,94 +7,105 @@ $(document).ready(function () {
 
 });
 
-//Estados
+// //Estados
 function obtenerListaEstados() {
-
-    $.ajax({
-        type: "GET",
-        dataType: "html",
-        url: "./ServletEstadoListar", //localhost:8090/api/status
-        success: function (result) {
-            let parsedResultEstados = JSON.parse(result);
-            if (parsedResultEstados !== false) {
-                mostrarEstado(parsedResultEstados);
-            } else {
-                console.log("Hubo un problema al llamar los datos de lista Estados");
-            }
+    fetch('http://localhost:8090/motorclinic/api/service-orders', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
-    });
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Hubo un problema al llamar los datos de lista Estados (ordenes)');
+            }
+            return response.json();
+        })
+        .then(parsedResult => {
+            if (parsedResult !== false) {
+                mostrarEstado(parsedResult);
+            } else {
+                console.log('No se recibieron datos válidos de la lista de estados (ordenes)');
+            }
+        })
+        .catch(error => {
+            console.error('Error al obtener la lista de estados (ordenes):', error);
+        });
 }
 
-
+//
 function mostrarEstado(listaEstados) {
     let accordeon = "";
+
     //moto es en realidad el id de la orden.
     $.each(listaEstados, function (index, estado) {
-        let estadoParsed = JSON.parse(estado);
-//       
+//
         accordeon += '<div class="accordion accordion-flush" id="accordionFlushEstados">' +
-                '<div class="accordion-item">' +
-                '<h2 class="accordion-header" id="flush-heading-estados-' + estadoParsed.idEstado + '">' +
-                '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-estados-' + estadoParsed.idEstado + '" aria-expanded="false" aria-controls="flush-collapse-estados-' + estadoParsed.idEstado + '">' +
-                '<strong>Estado:  #</strong> ' + estadoParsed.idEstado + ' de la orden: ' + estadoParsed.moto +
-                '</button>' +
-                '</h2>' +
-                '<div id="flush-collapse-estados-' + estadoParsed.idEstado + '" class="accordion-collapse collapse" aria-labelledby="flush-heading-estados-' + estadoParsed.idEstado + '" data-bs-parent="#accordionFlushEstados">';
+            '<div class="accordion-item">' +
+            '<h2 class="accordion-header" id="flush-heading-estados-' + estado.status.id + '">' +
+            '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-estados-' +
+            estado.status.id + '" aria-expanded="false" aria-controls="flush-collapse-estados-' + estado.status.id + '">' +
+            '<strong>Estado:  #</strong> ' + estado.status.id + ' de la orden: ' + estado.id +
+            '</button>' +
+            '</h2>' +
+            '<div id="flush-collapse-estados-' + estado.status.id + '" class="accordion-collapse collapse" aria-labelledby="flush-heading-estados-' +
+            estado.status.id + '" data-bs-parent="#accordionFlushEstados">';
         verEstadosPorOrden();
+
         function verEstadosPorOrden() {
             $.each(listaEstados, function (index, estadoPorO) {
-                let estadoOrdenParsed = JSON.parse(estadoPorO);
+
                 //solo poner las ordenes de está moto.
-                if (estadoOrdenParsed.moto === estadoParsed.moto) {
+                if (estadoPorO.id === estado.id) {
                     accordeon += '<div class="accordion-body">' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Kilometraje:  </strong>' + estadoOrdenParsed.kilometraje + '</li>' +
-                            '<li class="list-group-item"><strong>Indicadores:  </strong>' + estadoOrdenParsed.indicadores + '</li>' +
-                            '<li class="list-group-item"><strong>Detalle indicadores:  </strong>' + estadoOrdenParsed.desIndicadores + '</li>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Aceite:  </strong>' + estadoOrdenParsed.aceite + '</li>' +
-                            '<li class="list-group-item"><strong>Nivel aceite:  </strong>' + estadoOrdenParsed.nivelAceite + '</li>' +
-                            '<li class="list-group-item"><strong>Nivel de combustible  </strong>$' + estadoOrdenParsed.combustible + '</li>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Liquido de frenos:  </strong>' + estadoOrdenParsed.liquidoFrenos + '</li>' +
-                            '<li class="list-group-item"><strong>Liquido de embrague:  </strong>' + estadoOrdenParsed.liquidoEmbrague + '</li>' +
-                            '<li class="list-group-item"><strong>Liquido refrigerante:  </strong>' + estadoOrdenParsed.liquidoRefrigerante + '</li>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Luces:  </strong>' + estadoOrdenParsed.lucesAptas + '</li>' +
-                            '<li class="list-group-item"><strong>Espejos:  </strong>' + estadoOrdenParsed.espejos + '</li>' +
-                            '<li class="list-group-item"><strong>Claxon:  </strong>' + estadoOrdenParsed.claxon + '</li>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Tanque:  </strong>' + estadoOrdenParsed.tanque + '</li>' +
-                            '<li class="list-group-item"><strong>Llanta delantera:  </strong>' + estadoOrdenParsed.llantaDelantera + '</li>' +
-                            '<li class="list-group-item"><strong>Llanta trasera:  </strong>' + estadoOrdenParsed.llantaTrasera + '</li>' +
-                            '</ul>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Motor:  </strong>' + estadoOrdenParsed.motor + '</li>' +
-                            '<li class="list-group-item"><strong>Chasis:  </strong>' + estadoOrdenParsed.chasis + '</li>' +
-                            '<li class="list-group-item"><strong>Acelerador:  </strong>' + estadoOrdenParsed.acelerador + '</li>' +
-                            '</ul>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Escape:  </strong>' + estadoOrdenParsed.escape + '</li>' +
-                            '<li class="list-group-item"><strong>Trasmisión:  </strong>' + estadoOrdenParsed.trasmision + '</li>' +
-                            '<li class="list-group-item"><strong>Embrague:  </strong>' + estadoOrdenParsed.embrague + '</li>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Frenos:  </strong>' + estadoOrdenParsed.frenos + '</li>' +
-                            '<li class="list-group-item"><strong>Cadena:  </strong>' + estadoOrdenParsed.cadena + '</li>' +
-                            '<li class="list-group-item"><strong>Apoya pies:  </strong>' + estadoOrdenParsed.apoyaPies + '</li>' +
-                            '</ul>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>';
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Kilometraje:  </strong>' + estado.status.mileage + '</li>' +
+                        '<li class="list-group-item"><strong>Indicadores:  </strong>' + estado.status.indicators + '</li>' +
+                        '<li class="list-group-item"><strong>Detalle indicadores:  </strong>' + estado.status.indicatorsDesc + '</li>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Aceite:  </strong>' + estado.status.oil + '</li>' +
+                        '<li class="list-group-item"><strong>Nivel aceite:  </strong>' + estado.status.oilLevel + '</li>' +
+                        '<li class="list-group-item"><strong>Nivel de combustible  </strong>$' + estado.status.fuel + '</li>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Liquido de frenos:  </strong>' + estado.status.brakeFluid + '</li>' +
+                        '<li class="list-group-item"><strong>Liquido de embrague:  </strong>' + estado.status.clutchFluid + '</li>' +
+                        '<li class="list-group-item"><strong>Liquido refrigerante:  </strong>' + estado.status.coolant + '</li>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Luces:  </strong>' + estado.status.lightsGood + '</li>' +
+                        '<li class="list-group-item"><strong>Espejos:  </strong>' + estado.status.mirrors + '</li>' +
+                        '<li class="list-group-item"><strong>Claxon:  </strong>' + estado.status.horn + '</li>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Tanque:  </strong>' + estado.status.tank + '</li>' +
+                        '<li class="list-group-item"><strong>Llanta delantera:  </strong>' + estado.status.frontTire + '</li>' +
+                        '<li class="list-group-item"><strong>Llanta trasera:  </strong>' + estado.status.rearTire + '</li>' +
+                        '</ul>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Motor:  </strong>' + estado.status.engine + '</li>' +
+                        '<li class="list-group-item"><strong>Chasis:  </strong>' + estado.status.chassis + '</li>' +
+                        '<li class="list-group-item"><strong>Acelerador:  </strong>' + estado.status.throttle + '</li>' +
+                        '</ul>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Escape:  </strong>' + estado.status.exhaust + '</li>' +
+                        '<li class="list-group-item"><strong>Trasmisión:  </strong>' + estado.status.transmission + '</li>' +
+                        '<li class="list-group-item"><strong>Embrague:  </strong>' + estado.status.clutch + '</li>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Frenos:  </strong>' + estado.status.brakes + '</li>' +
+                        '<li class="list-group-item"><strong>Cadena:  </strong>' + estado.status.chain + '</li>' +
+                        '<li class="list-group-item"><strong>Apoya pies:  </strong>' + estado.status.footPegs + '</li>' +
+                        '</ul>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>';
                     try {
-                        clase = ".orden-" + estadoParsed.moto;
+                        clase = ".orden-" + estado.id;
                         $(clase).html(accordeon);
                     } catch (e) {
                         console.log(e);
@@ -106,22 +117,32 @@ function mostrarEstado(listaEstados) {
     });
 }
 
-//Registros
+//
+// //Registros
 function obtenerListaRegistros() {
-
-    $.ajax({
-        type: "GET",
-        dataType: "html",
-        url: "./ServletRegistroListar",
-        success: function (result) {
-            let parsedResultRegistros = JSON.parse(result);
+    fetch('http://localhost:8090/motorclinic/api/records', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                console.log(response)
+                throw new Error('Hubo un problema al llamar los datos de lista registros producto-servicio');
+            }
+            return response.json();
+        })
+        .then(parsedResultRegistros => {
             if (parsedResultRegistros !== false) {
                 mostrarRegistro(parsedResultRegistros);
             } else {
-                console.log("Hubo un problema al llamar los datos de lista registros producto-servicio");
+                console.log('No se recibieron datos válidos de la lista de registros producto-servicio');
             }
-        }
-    });
+        })
+        .catch(error => {
+            console.error('Error al obtener la lista de registros producto-servicio:', error);
+        });
 }
 
 
@@ -132,42 +153,44 @@ function mostrarRegistro(listaRegistros) {
         let registroParsed = JSON.parse(registro);
 //       1 2 3/ 3
         accordeonR += '<div class="cajitas ">' +
-                '<div id="ordenes" class="ordenes modal-body border border border-3 border-dark" style="--bs-border-opacity: .4;"><strong> Registros de la orden</strong>';
+            '<div id="ordenes" class="ordenes modal-body border border border-3 border-dark" style="--bs-border-opacity: .4;"><strong> Registros de la orden</strong>';
 
         verRegistrosPorOrden();
+
         function verRegistrosPorOrden() {
             $.each(listaRegistros, function (index, registroPorO) {
                 let registroOrdenParsed = JSON.parse(registroPorO);
                 //solo poner las ordenes de está moto.
                 if (registroOrdenParsed.idOrden === registroParsed.idOrden) {
                     accordeonR += '<div class="accordion accordion-flush" id="accordionFlushRegistros">' +
-                            '<div class="accordion-item">' +
-                            '<h2 class="accordion-header" id="flush-heading-registros-' + registroOrdenParsed.idRegistro + '">' +
-                            '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-registros-' + registroOrdenParsed.idRegistro + '" aria-expanded="false" aria-controls="flush-collapse-registros-' + registroOrdenParsed.idRegistro + '">' +
-                            '<strong>Registro:  #</strong> ' + registroOrdenParsed.idRegistro + ' de la orden: ' + registroOrdenParsed.idOrden +
-                            '</button>' +
-                            '</h2>' +
-                            '<div id="flush-collapse-registros-' + registroOrdenParsed.idRegistro + '" class="accordion-collapse collapse" aria-labelledby="flush-heading-registros-' + registroOrdenParsed.idRegistro + '" data-bs-parent="#accordionFlushRegistros">' +
-                            '<div class="accordion-body">' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Producto:  </strong>' + registroOrdenParsed.nombreProducto + '</li>' +
-                            '<li class="list-group-item"><strong>Valor producto:  </strong>' + registroOrdenParsed.valorProducto + '</li>' +
-                            '<li class="list-group-item"><strong>Detalle Servicio:  </strong>' + registroOrdenParsed.nombreServicio + '</li>' +
-                            '</ul>' +
-                            '<ul class="list-group list-group">' +
-                            '<li class="list-group-item"><strong>Detalle servicio:  </strong>' + registroOrdenParsed.detalleServicio + '</li>' +
-                            '<li class="list-group-item"><strong>Valor servicio:  </strong>' + registroOrdenParsed.valorServicio + '</li>' +
-                            '<li class="list-group-item"><strong>Aprobado por cliente.  </strong>$' + registroOrdenParsed.aprobado + '</li>' +
-                            '</ul>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>';
+                        '<div class="accordion-item">' +
+                        '<h2 class="accordion-header" id="flush-heading-registros-' + registroOrdenParsed.idRegistro + '">' +
+                        '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-registros-' + registroOrdenParsed.idRegistro + '" aria-expanded="false" aria-controls="flush-collapse-registros-' + registroOrdenParsed.idRegistro + '">' +
+                        '<strong>Registro:  #</strong> ' + registroOrdenParsed.idRegistro + ' de la orden: ' + registroOrdenParsed.idOrden +
+                        '</button>' +
+                        '</h2>' +
+                        '<div id="flush-collapse-registros-' + registroOrdenParsed.idRegistro + '" class="accordion-collapse collapse" aria-labelledby="flush-heading-registros-' + registroOrdenParsed.idRegistro + '" data-bs-parent="#accordionFlushRegistros">' +
+                        '<div class="accordion-body">' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Producto:  </strong>' + registroOrdenParsed.nombreProducto + '</li>' +
+                        '<li class="list-group-item"><strong>Valor producto:  </strong>' + registroOrdenParsed.valorProducto + '</li>' +
+                        '<li class="list-group-item"><strong>Detalle Servicio:  </strong>' + registroOrdenParsed.nombreServicio + '</li>' +
+                        '</ul>' +
+                        '<ul class="list-group list-group">' +
+                        '<li class="list-group-item"><strong>Detalle servicio:  </strong>' + registroOrdenParsed.detalleServicio + '</li>' +
+                        '<li class="list-group-item"><strong>Valor servicio:  </strong>' + registroOrdenParsed.valorServicio + '</li>' +
+                        '<li class="list-group-item"><strong>Aprobado por cliente.  </strong>$' + registroOrdenParsed.aprobado + '</li>' +
+                        '</ul>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>';
 
                 } //termina if estados por ordenes
             });//foreach
         }
-        accordeonR +='</div>';
+
+        accordeonR += '</div>';
         try {
             clase = ".orden-registro-" + registroParsed.idOrden;
             $(clase).html(accordeonR);
